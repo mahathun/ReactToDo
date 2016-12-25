ef9e9d276b4b1efd4ae810a28565cba8f9ef074e
dan mahavithana <mahathun.online@gmail.com>
2016-12-25 23:19:33 +1300

    Added main container TodoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 
 
 //loading foundation
@@ -12,6 +14,8 @@ require('style!css!sass!applicationStyles');
 
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p> ,
+  <div>
+    <TodoApp />
+  </div>,
   document.getElementById('app')
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var TodoApp = React.createClass({
+  render:function () {
+    return (
+      <div>
+        Todo App Main container
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
